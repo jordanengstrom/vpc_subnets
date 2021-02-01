40667185aae08674690dbb4eddfb58e39b162f13
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-01 13:13:26 -0700

    first iteration of first iteration complete

--- first_iteration.py
+++ first_iteration.py
@@ -11,30 +11,27 @@ import sys
 
 def main():
     # TODO: get CIDR blocks (CIDR = Classless Inter-Domain Routing)
-    if len(sys.argv) < 2:
-        print("Please supply a VPC id as an argument!")
-    else:
-        vpc_id = sys.argv[1]
-        print("VPC ID:", vpc_id)
+    print('Please supply a VPC ID: ')
+    vpc_id = input()
 
-        # Get EC2 resource
-        ec2 = boto3.resource('ec2')
+    print("Using VPC ID:", vpc_id)
 
-        # Get VPC resource using Ec2 resource by supplying VPC ID
-        vpc = ec2.Vpc(vpc_id)
-        print("VPC CIDR:", vpc.cidr_block)
+    # Get EC2 resource
+    ec2 = boto3.resource('ec2')
 
-        # Check whether VPC has subnets and display them accordingly
-        subnets = list(vpc.subnets.all())
-        if len(subnets) > 0:
-            print("\nSubnets:")
-            for subnet in subnets:
-                print(subnet.id, "-", subnet.cidr_block)
-        else:
-            print("There is no subnet in this VPC!")
+    # Get VPC resource using Ec2 resource by supplying VPC ID
+    vpc = ec2.Vpc(vpc_id)
+    print("VPC CIDR:", vpc.cidr_block)
 
+    # Check whether VPC has subnets and display them accordingly
+    subnets = list(vpc.subnets.all())
     # TODO: print available subnets
-    pass
+    if len(subnets) > 0:
+        print("\nSubnets:")
+        for subnet in subnets:
+            print(subnet.id, "-", subnet.cidr_block)
+    else:
+        print("There is no subnet in this VPC!")
 
 
 if __name__ == '__main__':
