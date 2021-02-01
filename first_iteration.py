d3f52d371f35e14ebad6f6aec2e4f952badc24f6
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-01 14:04:15 -0700

    separated main out into two different functions

--- first_iteration.py
+++ first_iteration.py
@@ -4,28 +4,33 @@ Create an executable script that will use the AWS CLI to print the CIDR blocks o
 in it.
 """
 
-import awscliv2
 import boto3
-import sys
 
 
-def main():
-    # TODO: get CIDR blocks (CIDR = Classless Inter-Domain Routing)
+def get_vpc_from_user():
     print('Please supply a VPC ID: ')
     vpc_id = input()
-
     print("Using VPC ID:", vpc_id)
+    return vpc_id
 
-    # Get EC2 resource
+
+def get_vpc_cidr(_vpc_id: str):
+    # Declare ec2 resource
     ec2 = boto3.resource('ec2')
 
     # Get VPC resource using Ec2 resource by supplying VPC ID
-    vpc = ec2.Vpc(vpc_id)
+    vpc = ec2.Vpc(_vpc_id)
     print("VPC CIDR:", vpc.cidr_block)
+    return vpc.cidr_block, vpc
+
+
+def main():
+    # Get CIDR blocks => (CIDR = Classless Inter-Domain Routing)
+    vpc_id = get_vpc_from_user()
+    cidr_block, vpc = get_vpc_cidr(vpc_id)
 
     # Check whether VPC has subnets and display them accordingly
     subnets = list(vpc.subnets.all())
-    # TODO: print available subnets
     if len(subnets) > 0:
         print("\nSubnets:")
         for subnet in subnets:
